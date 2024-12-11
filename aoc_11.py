b399b345927f51e77ea8f1d1a0ef11014c8e9776
tcatsuko <58714618+tcatsuko@users.noreply.github.com>
2024-12-11 10:14:37 -0600

    Day 11: Plutonian Pebbles

--- aoc_11.py
+++ aoc_11.py
@@ -1,22 +1,33 @@
+from collections import defaultdict
 f = open('aoc11.txt','rt')
-stones = []
 for line in f:
-    stones = [int(x) for x in line[:-1].split()]
+    stones_list = [int(x) for x in line[:-1].split()]
 f.close()
 num_blinks = 75
 
-for _ in range(num_blinks):
-    new_stones = []
+stones = defaultdict(int)
+for stone in stones_list:
+    stones[stone] += 1
+    
+for blink in range(num_blinks):
+    if blink == 25: # 25th blink
+        print('Part 1: there are ' + str(num_of_stones) + ' stones now after ' + str(blink) + ' blinks.')
+    new_stones = defaultdict(int)
+    
     for stone in stones:
+        num_of_stones = stones[stone]
+        
+        
         if stone == 0:
-            new_stones += [1]
+            new_stones[1] += num_of_stones
         elif len(str(stone)) % 2 == 0:
             stone_string = str(stone)
-            stone_string_halflife = len(stone_string) // 2
-            new_stones += [int(stone_string[:stone_string_halflife])]
-            new_stones += [int(stone_string[stone_string_halflife:])]
+            stone_string_length = len(stone_string) // 2
+            new_stones[int(stone_string[:stone_string_length])] += num_of_stones
+            new_stones[int(stone_string[stone_string_length:])] += num_of_stones
         else:
-            new_stones += [stone * 2024]
-    stones = new_stones[:]
+            new_stones[stone * 2024] += num_of_stones
+    stones = new_stones.copy()
     db = 1
-print('Part 1: there are ' + str(len(stones)) + ' stones now after ' + str(num_blinks) + ' blinks.')
+    num_of_stones = sum(stones.values())
+print('Part 2: there are ' + str(num_of_stones) + ' stones now after ' + str(num_blinks) + ' blinks.')
