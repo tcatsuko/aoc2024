5ec876299bd80fa4b38ac2d758dfa8c9eab28e3f
tcatsuko <58714618+tcatsuko@users.noreply.github.com>
2024-12-20 11:14:08 -0600

    Day 20: Race Condition

--- aoc_20.py
+++ aoc_20.py
@@ -1,5 +1,4 @@
 import networkx as nx
-from collections import defaultdict
 
 f = open('aoc20.txt','rt')
 raw_input = []
@@ -7,6 +6,7 @@ for line in f:
     raw_input += [line[:-1]]
 f.close()
 G = nx.Graph()
+
 def manhattan_distance(n1, n2):
     return abs(n1[0] - n2[0]) + abs(n1[1]-n2[1])
 
@@ -26,40 +26,22 @@ for row, line in enumerate(raw_input):
             if raw_input[row][col+1] != '#':
                 G.add_edge((row, col), (row,col+1))
 shortest_path = list(nx.shortest_path(G, start_node, end_node))
-cheats = defaultdict(int)
-cheat_distance = 2
+p1_cheat_distance = 2
+p2_cheat_distance = 20
+p1_cheats = 0
+p2_cheats = 0
+goal_time_saved = 100
 for i, node in enumerate(shortest_path):
-    possible_cheats = [x for x in shortest_path[i:] if manhattan_distance(node, x) <= cheat_distance and manhattan_distance(node, x) > 0]
+    possible_cheats = [x for x in shortest_path[i:] if manhattan_distance(node, x) <= p2_cheat_distance and manhattan_distance(node, x) > 0]
     for next_node in possible_cheats:
+        distance = manhattan_distance(node, next_node)
         next_index = shortest_path.index(next_node)
-        time_saved = abs(i - next_index) - manhattan_distance(node, next_node)
+        time_saved = abs(i - next_index) - distance
+        if time_saved >= goal_time_saved:
+            if distance == p1_cheat_distance:
+                p1_cheats += 1
+            p2_cheats += 1
        
-        cheats[time_saved] += 1
-# NOTE: cheat counts will be doubled since we are counting from both start and end nodes
-goal_time_saved = 100
-goal_cheats = [x for x in cheats.keys() if x >= goal_time_saved]
-num_goal_cheats = 0
-for item in goal_cheats:
-    num_goal_cheats += cheats[item]
-print('Part 1: there are ' + str(num_goal_cheats) + ' cheats that save at least ' + str(goal_time_saved) + ' seconds.')
-
-cheats = defaultdict(int)
-# Part 2
-cheat_distance = 20
-for i, node in enumerate(shortest_path):
-    
-    possible_cheats = [x for x in shortest_path[i:] if manhattan_distance(node, x) <= cheat_distance and manhattan_distance(node, x) > 0]
-    for next_node in possible_cheats:
-        next_index = shortest_path.index(next_node)
-        time_saved = abs(i - next_index) - manhattan_distance(node, next_node)
-        
-        cheats[time_saved] += 1
-    print(str(i) + '/' + str(len(shortest_path)))
-# NOTE: cheat counts will be doubled since we are counting from both start and end nodes
-goal_time_saved = 100
-goal_cheats = [x for x in cheats.keys() if x >= goal_time_saved]
-num_goal_cheats = 0
-for item in goal_cheats:
-    num_goal_cheats += cheats[item]
-print('Part 2: there are ' + str(num_goal_cheats) + ' cheats that save at least ' + str(goal_time_saved) + ' seconds.')
+print('Part 1: there are ' + str(p1_cheats) + ' cheats that save at least ' + str(goal_time_saved) + ' seconds.')
+print('Part 2: there are ' + str(p2_cheats) + ' cheats that save at least ' + str(goal_time_saved) + ' seconds.')
 
